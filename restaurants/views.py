8c05fb7f3d36eea8dde7bcc67b3720f023fe3c88
shorookMS <shorook.m.saleh@gmail.com>
2018-10-14 12:42:24 +0300

    Task Submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Restaurant, Item
 from .forms import RestaurantForm, ItemForm, SignupForm, SigninForm
 from django.contrib.auth import login, authenticate, logout
-
+from django.db.models import Q
 def no_access(request):
     return render(request, 'no_access.html')
 
@@ -46,8 +46,17 @@ def signout(request):
     return redirect("signin")
 
 def restaurant_list(request):
+    restaurants = Restaurant.objects.all()
+    query = request.GET.get("q")
+    if query:
+        restaurants = restaurants.filter(
+            Q(owner__username__icontains=query)|
+            Q(name__icontains=query)|
+            Q(description__icontains=query)
+            ).distinct()
+
     context = {
-        "restaurants":Restaurant.objects.all()
+        "restaurants":restaurants
     }
     return render(request, 'list.html', context)
 
